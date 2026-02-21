0530e4408e95b32e9c953caa6ed01fa13ed4e42e
Yunqi Ji <yunqiji.dev@gmail.com>
2026-02-21 23:20:48 +0800

    feat(courses): add students.py for p06

---
+++ 01-foundational-courses/p06-file/students.py
@@ -0,0 +1,101 @@
+# Example 1
+with open("students.csv") as file:
+    for line in file:
+        row = line.rstrip().split(",")
+        print(f"{row[0]} is in {row[1]}")
+
+
+# Example 2
+with open("students.csv") as file:
+    for line in file:
+        name, house = line.rstrip().split(",")
+        print(f"{name} is in {house}")
+
+
+# Example 3
+students = []
+
+with open("students.csv") as file:
+    for line in file:
+        name, house = line.rstrip().split(",")
+        students.append(f"{name} is in {house}")
+
+for student in sorted(students):
+    print(student)
+
+
+# Example 4
+student = []
+
+with open("students.csv") as file:
+    for line in file:
+        name, house = line.rstrip().split(",")
+        studnet = {}
+        student["name"] = name
+        student["house"] = house
+        student.append(studnet)
+
+for studnet in students:
+    print(f"{studnet['name']} is in {student['house']}")
+
+
+# Example 4
+student = []
+
+with open("students.csv") as file:
+    for line in file:
+        name, house = line.rstrip().split(",")
+        studnet = {"name": name, "house": house}
+        student.append(studnet)
+
+for studnet in students:
+    print(f"{studnet['name']} is in {student['house']}")
+
+
+# Example 5
+student = []
+
+with open("students.csv") as file:
+    for line in file:
+        name, house = line.rstrip().split(",")
+        studnet = {"name": name, "house": house}
+        student.append(studnet)
+
+
+def get_name(stundent):
+    return student["name"]
+
+
+for studnet in sorted(students, key=get_name, reverse=True):
+    print(f"{studnet['name']} is in {student['house']}")
+
+
+# Example 6
+student = []
+
+with open("students.csv") as file:
+    for line in file:
+        name, house = line.rstrip().split(",")
+        studnet = {"name": name, "house": house}
+        student.append(studnet)
+
+
+def get_house(stundent):
+    return student["house"]
+
+
+for studnet in sorted(students, key=get_house):
+    print(f"{studnet['name']} is in {student['house']}")
+
+
+# Example 7
+student = []
+
+with open("students.csv") as file:
+    for line in file:
+        name, house = line.rstrip().split(",")
+        studnet = {"name": name, "house": house}
+        student.append(studnet)
+
+for studnet in sorted(students, key=lambda student: student["name"]):
+    print(f"{studnet['name']} is in {student['house']}")
