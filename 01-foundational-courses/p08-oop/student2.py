e9f556f9971b447c910e2540491aa41cb1de264c
Yunqi Ji <yunqiji.dev@gmail.com>
2026-03-28 23:27:14 +0800

    feat(courses): expand student2.py for p08

--- 01-foundational-courses/p08-oop/student2.py
+++ 01-foundational-courses/p08-oop/student2.py
@@ -1,7 +1,8 @@
 # Example 1
+"""
 class Student:
-    ...
-
+    name:str =""
+    house:str =""
 
 def main():
     student = get_student()
@@ -12,6 +13,29 @@ def get_student():
     student = Student()
     student.name = input("Name: ")
     student.house = input("House: ")
+    return student
+
+if __name__ == "__main__":
+    main()
+    """
+
+
+# Example 2
+class Student:
+    def __init__(self):
+        self.name = name
+        self.house = house
+
+def main():
+    student = get_student()
+    print(f"{student.name} from {student.house}")
+
+
+def get_student():
+    name = input("Name:")
+    house = input("House:")
+    student = Student(name, house)
+
     return student
 
 if __name__ == "__main__":
