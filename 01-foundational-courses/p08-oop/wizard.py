fb7b28624c3043437233220df481b981889d531b
Yunqi Ji <yunqiji.dev@gmail.com>
2026-04-28 22:04:16 +0800

    feat(courses): complete wizard.py for p08

---
+++ 01-foundational-courses/p08-oop/wizard.py
@@ -0,0 +1,59 @@
+"""class Wizard:
+    def __init__(self, name,):
+        if not name:
+            raise ValueError("Missing name")
+        self.name  = name
+
+    ...
+        
+
+
+class Student:
+    def __init__(self, name, house):
+        if not name:
+            raise ValueError("Missing name")
+        self.name = name
+        self.house = house
+
+    ...
+
+
+class Professor:
+    def __init__(self, name, subject):
+        if not name:
+            raise ValueError("Missing name")
+        self.name = name
+        self.subject = subject
+
+    ..."""
+
+
+class Wizard:
+    def __init__(self, name,):
+        if not name:
+            raise ValueError("Missing name")
+        self.name  = name
+
+    ...
+        
+
+
+class Student(Wizard):
+    def __init__(self, name, house):
+        super().__init__(name)
+        self.house = house
+
+    ...
+
+
+class Professor(Wizard):
+    def __init__(self, name, subject):
+        super().__init__(name)
+        self.subject = subject
+
+    ...
+
+
+wizard = Wizard("Albus")
+student = Student("Harry", "Gryffindor")
+professor = Professor("Severus", "Defense Against The Dark Arts")
