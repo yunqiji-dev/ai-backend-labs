42802b12806f843156453263c1d8ccfc2f401045
Yunqi Ji <yunqiji.dev@gmail.com>
2026-04-28 23:28:21 +0800

    feat(courses): complete vault.py for p08

--- 01-foundational-courses/p08-oop/vault.py
+++ 01-foundational-courses/p08-oop/vault.py
@@ -1,3 +1,5 @@
+# Example 1
+"""
 class Vault:
     def __init__(self, galleons=0, sickles=0, knuts=0):
         self.gallenons = galleons
@@ -6,7 +8,6 @@ class Vault:
 
     def __str__(self):
         return f"{self.gallenons} Gallenons, {self.sickles} Sickles, {self.knuts} Knuts"
-
 
 
 potter = Vault(100, 50, 25)
@@ -20,4 +21,31 @@ sickles = potter.sickles + weasley.sickles
 knuts = potter.knuts + weasley.knuts
 
 total = Vault(galleons, sickles, knuts)
+print(total)
+"""
+
+
+# Example 2
+class Vault:
+    def __init__(self, galleons=0, sickles=0, knuts=0):
+        self.gallenons = galleons
+        self.sickles = sickles
+        self.knuts = knuts
+
+    def __str__(self):
+        return f"{self.gallenons} Gallenons, {self.sickles} Sickles, {self.knuts} Knuts"
+
+    def __add__(self, other ):
+        galleons = self.gallenons + other.gallenons
+        sickles = self.sickles + other.sickles
+        knuts = self.knuts + other.knuts
+        return Vault(galleons, sickles, knuts)
+
+potter = Vault(100, 50, 25)
+print(potter)
+
+weasley = Vault(25, 50, 100)
+print(weasley)
+
+total = potter + weasley
 print(total)
